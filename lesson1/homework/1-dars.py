6a5c8aa701b8b0540f6c8e98c5ed37ae7fd75151
abbos-0101 <abbosgulomjonov77@gmail.com>
2025-06-10 12:26:41 +0500

    Create 1-dars.py

---
+++ lesson1/homework/1-dars.py
@@ -0,0 +1,51 @@
+# Homework 1
+
+# task 1
+
+"""
+tomon = float(input("Kvadratning tomon uzunligini kiriting: "))
+
+perimetr = 4 * tomon
+
+yuzasi = tomon * tomon
+print("Kvadratning perimetri:", perimetr)
+print("Kvadratning yuzasi:", yuzasi)
+"""
+
+# task 2
+"""
+d = float(input("Doiraning diametrini kiriting: d = "))
+
+L = d * 3.14
+
+print(" Doira Aylanasining uzunligi: ", L)
+"""
+
+#task 3
+"""
+a = float(input(" a sonini kiriting: a = "))
+b = float(input(" b sonini kiriting: b = "))
+
+S = (a + b) / 2
+
+print(" O'rtacha qiymati: S = ", S)
+
+"""
+
+# task 4
+"""
+a = float(input(" a sonini kiriting: a = "))
+b = float(input(" b sonini kiriting: b = "))
+
+L = a + b
+S = a * b
+
+M = a * a
+N = b * b
+
+print(" Sonlarning yig'indisi: L = ", L)
+print(" Sonlarning ko'paytmasi: S = ", S)
+print(" a sonining kvadrati: M = ", M)
+print(" b sonining kvadrati: N = ", N)
+
+"""
